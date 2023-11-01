e28fefa431277d1da945ae68b32a31856c8370b4
Carlos Eduardo <carlosedutis@outlook.com>
2023-11-01 20:35:38 -0300

    feat: Adiciona configuração do ESLint
    Este commit adiciona a configuração do ESLint ao projeto para manter a ordem e coerência do código. Ele também está relacionado ao exercício 3.22.

--- index.js
+++ index.js
@@ -1,113 +1,114 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const app = express();
-const Person = require("./models/person");
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+const Person = require('./models/person')
 
-morgan.token("req-body", (req) => JSON.stringify(req.body));
-app.use(morgan(":method :url :status :response-time ms - :req-body"));
-app.use(cors());
-app.use(express.static("build"));
+morgan.token('req-body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :response-time ms - :req-body'))
+app.use(cors())
+app.use(express.static('build'))
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
-    response.json(persons);
-  });
-});
+    response.json(persons)
+  })
+})
 
-app.use(express.json());
+app.use(express.json())
 
-app.get("/info", (request, response) => {
-  const date = new Date();
+app.get('/info', (request, response) => {
+  const date = new Date()
 
   const countPerson = () => {
     Person.estimatedDocumentCount({}, (err, count) => {
       if (err) {
-        console.error(err);
+        console.error(err)
       } else {
-        console.log(count);
-        return count;
+        console.log(count)
+        return count
       }
-    });
-  };
+    })
+  }
   const infoText = `
   <p>Phonebook has info for ${countPerson()} people</p>
-    <p>${date}</p>`;
-  response.send(infoText);
-});
+    <p>${date}</p>`
+  response.send(infoText)
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
     .catch((error) => {
       next(error)
-      
-    });
-});
 
-app.delete("/api/persons/:id", (request, response, next) => {
+    })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then((result) => {
-      response.status(202).end();
+    .then(() => {
+      response.status(202).end()
     })
     .catch((error) => {
-      next(error);
-    });
-});
+      next(error)
+    })
+})
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
   const person = {
     name: body.name,
     number: body.number,
-  };
+  }
 
   Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then((updatedPerson) => {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(404).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(404).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const port = process.env.PORT;
+// eslint-disable-next-line no-undef
+const port = process.env.PORT
 app.listen(port, () => {
-  console.log(`Server Running on port ${port}`);
-});
+  console.log(`Server Running on port ${port}`)
+})
